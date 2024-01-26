984b40bc9685be1f211b7197d9cfdd8643a34186
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-01-26 15:28:49 +0200

    add implementation bank account

---
+++ src/bankAccount/Main.java
@@ -0,0 +1,29 @@
+package bankAccount;
+
+import exceptions.InsufficientFundsException;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int accountBalance = 10000;
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Введіть суму для оплати: ");
+            double amount = scanner.nextDouble();
+
+            if (amount > accountBalance) {
+                throw new InsufficientFundsException("Недостатньо коштів на карті.");
+            } else {
+                accountBalance -= amount;
+                System.out.println("Оплата успішна. Залишковий баланс: " + accountBalance);
+            }
+        } catch (InsufficientFundsException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Неправильні дані. Введіть дійсну суму.");
+        }
+    }
+}
--- src/financialExchange/Main.java
+++ src/financialExchange/Main.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         double[] prices = {10.0, 2.1, 15.4, 240.0, 17.7};
+        String currency = "USD";
         Scanner scanner = new Scanner(System.in);
 
         try {
-            System.out.println("Введіть індекс пошуку ціни: ");
+            System.out.print("Введіть індекс пошуку ціни: ");
             int index = scanner.nextInt();
 
-            if(index>=0 && index< prices.length){
-                System.out.println("Ціна в індексі " + index + ": " + prices[index]);
-            }else {
+            if (index >= 0 && index < prices.length) {
+                System.out.print("Ціна в індексі " + index + " = " + prices[index] + " " + currency);
+            } else {
                 System.out.println("Межа пошуку від 0 до " + prices.length + ". Введіть в межах пошуку.");
             }
         } catch (Exception e) {
