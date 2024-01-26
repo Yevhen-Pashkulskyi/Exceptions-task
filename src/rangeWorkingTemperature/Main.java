c08156ee302deddf3f3670d94076cb4e0bef43a6
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-01-26 15:44:24 +0200

    add implementation range working temperature

---
+++ src/rangeWorkingTemperature/Main.java
@@ -0,0 +1,31 @@
+package rangeWorkingTemperature;
+
+import exceptions.TemperatureOutOfRangeException;
+
+import java.util.Scanner;
+
+public class Main {
+    private static final int MIN_TEMPERATURE = -10;
+    private static final int MAX_TEMPERATURE = 35;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Введіть поточну температуру: ");
+            int temperature = scanner.nextInt();
+
+            if (temperature < -MIN_TEMPERATURE || temperature > MAX_TEMPERATURE) {
+                throw new TemperatureOutOfRangeException("Пристрій не може працювати поза діапазоном температур від " +
+                        MIN_TEMPERATURE + " до +" + MAX_TEMPERATURE);
+
+            } else {
+                System.out.println("Прилад працює, температура: " + temperature);
+            }
+        } catch (TemperatureOutOfRangeException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Неправильні дані. Введіть дійсну температуру.");
+        }
+    }
+}
