7c676b376a03122b4bc8196470b611e07720c06f
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-08-18 23:33:56 +0300

    added NumberFormatException

--- src/main/java/org/example/newVersion/controller/AppController.java
+++ src/main/java/org/example/newVersion/controller/AppController.java
@@ -4,11 +4,19 @@ import org.example.newVersion.model.InputData;
 import org.example.newVersion.view.AppView;
 
 public class AppController {
-    public void run (){
+    public void run() {
         AppView view = new AppView();
         InputData inputData = new InputData();
         view.menu();
-        inputData.getData();
+        validateNumber(new InputData().getData());
     }
 
+    private void validateNumber(String number) {
+        try {
+            Integer.parseInt(number);
+            System.out.println(number);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number " + number);
+        }
+    }
 }
---
+++ src/main/java/org/example/newVersion/exception/NumberException.java
@@ -0,0 +1,7 @@
+package org.example.newVersion.exception;
+
+public class NumberException extends NumberFormatException {
+    public NumberException(String message) {
+        super(message);
+    }
+}
