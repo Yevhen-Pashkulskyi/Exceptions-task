e2c51a4a93c1830a12629f8243e5599a64fc7fe6
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-08-25 15:33:26 +0300

    test key

--- src/main/java/org/example/v1_0_1/App.java
+++ src/main/java/org/example/v1_0_1/App.java
@@ -4,6 +4,6 @@ import org.example.v1_0_1.controller.AppController;
 
 public class App {
     public static void main(String[] args) {
-        new AppController().run();
+        new AppController().run();//
     }
 }
